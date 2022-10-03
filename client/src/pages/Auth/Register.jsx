60a72b91543056c0af0a74ac89558a2eef71ef47
nipengg <nevillecorneliustj@gmail.com>
2022-10-03 20:03:04 +0700

    create login and register page

---
+++ client/src/pages/Auth/Register.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+const Register = () => {
+    return (
+        <div className="Auth-form-container">
+            <form className="Auth-form">
+                <div className="Auth-form-content">
+                    <h3 className="Auth-form-title">Sign Up</h3>
+                    <div className="form-group mt-3">
+                        <label>Name</label>
+                        <input
+                            type="text"
+                            className="form-control mt-1"
+                            placeholder="Enter name"
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Email address</label>
+                        <input
+                            type="email"
+                            className="form-control mt-1"
+                            placeholder="Enter email"
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            className="form-control mt-1"
+                            placeholder="Enter password"
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Confirm Password</label>
+                        <input
+                            type="password"
+                            className="form-control mt-1"
+                            placeholder="Enter Confirm password"
+                        />
+                    </div>
+                    <div className="d-grid gap-2 mt-3">
+                        <button type="submit" className="btn btn-primary">
+                            Submit
+                        </button>
+                    </div>
+                    <p className="forgot-password text-right mt-2">
+                        Already have an account?<a href="/login">Login</a>
+                    </p>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Register
