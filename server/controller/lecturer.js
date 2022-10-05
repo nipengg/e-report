02db642e842cc50857cb05dda4b31571d7acb2e2
Vincent Sasmito <vincentsasmito@gmail.com>
2022-10-05 23:11:11 +0700

    Filled controller with queries for search

--- server/controller/lecturer.js
+++ server/controller/lecturer.js
@@ -1,12 +1,64 @@
 const { Lecturer } = require('../models')
+const { Op } = require('sequelize')
 const Validator = require('fastest-validator')
 
 const v = new Validator()
 
 const getLecturer = async (req, res) => {
     try {
-        const lecturers = await Lecturer.findAll();
-        return res.json({ data: lecturers })
+        const page = parseInt(req.query.page) || 0;
+        const limit = parseInt(req.query.limit) || 10;
+        const search = req.query.search || '';
+        const offset = limit * page;
+
+        const totalRows = await Lecturer.count({
+            where: {
+                [Op.or]: [
+
+                    {
+                        lecturer_id: {
+                            [Op.like]: '%' + search + '%'
+                        },
+                    },
+                    {
+                        lecturer_name: {
+                            [Op.like]: '%' + search + '%'
+                        },
+                    }
+                ]
+            }
+        });
+
+        const totalPage = Math.ceil(totalRows / limit);
+        const lecturers = await Lecturer.findAll({
+            where: {
+                [Op.or]: [
+
+                    {
+                        lecturer_id: {
+                            [Op.like]: '%' + search + '%'
+                        },
+                    },
+                    {
+                        lecturer_name: {
+                            [Op.like]: '%' + search + '%'
+                        }
+                    }
+                ]
+            },
+            offset: offset,
+            limit: limit,
+            order: [
+                ['lecturer_id', 'ASC'],
+            ] 
+        });
+        return res.json({ 
+            data: lecturers,
+            page: page,
+            limit: limit,
+            totalRows: totalRows,
+            totalPage: totalPage,
+        })
     } catch (error) {
         res.status(404).json({ message: error.message })
     }
--- server/controller/scores.js
+++ server/controller/scores.js
@@ -1,11 +1,58 @@
 const { Score, Student, Course } = require('../models')
+const { Op } = require('sequelize')
 const Validator = require('fastest-validator')
 
 const v = new Validator()
 
 const getScore = async (req, res) => {
     try {
+        const page = parseInt(req.query.page) || 0;
+        const limit = parseInt(req.query.limit) || 10;
+        const search = req.query.search || '';
+        const offset = limit * page;
+
+        const totalRows = await Score.count({
+            where: {
+                [Op.or]: [
+                    {
+                        id: {
+                            [Op.like]: '%' + search + '%'
+                        },
+                    },
+                    {
+                        nim: {
+                            [Op.like]: '%' + search + '%'
+                        }
+                    }
+                ]
+            },
+            include: [{
+                model: Student,
+                as: "student",
+            },
+            {
+                model: Course,
+                as: "course"
+            }],
+        });
+
+        const totalPage = Math.ceil(totalRows / limit);
+
         const scores = await Score.findAll({
+            where: {
+                [Op.or]: [
+                    {
+                        id: {
+                            [Op.like]: '%' + search + '%'
+                        },
+                    },
+                    {
+                        nim: {
+                            [Op.like]: '%' + search + '%'
+                        }
+                    }
+                ]
+            },
             include: [{
                 model: Student,
                 as: "student",
@@ -15,7 +62,13 @@ const getScore = async (req, res) => {
                 as: "course"
             }],
         })
-        return res.json({ data: scores })
+        return res.json({ 
+            data: scores,
+            page: page,
+            limit: limit,
+            totalRows: totalRows,
+            totalPage: totalPage,
+         })
     } catch (error) {
         res.status(404).json({ message: error.message })
     }
